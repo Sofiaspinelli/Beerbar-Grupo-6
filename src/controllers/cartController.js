498f4fa5d53c49729bbe4c4430713c9fa68c3bbf
nachorm96 <Rubio.nacho@yahoo.com.ar>
2022-08-12 18:12:45 -0300

    agregando json cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,5 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
+const save = (dato) => fs.writeFileSync(path.join(__dirname, '../data/carrito.json'),JSON.stringify(dato,null,4),'utf-8')
 module.exports = {
     cart : (req , res) => {
         res.render ('cart')
     },
+    productosSeleccionados: (req,res) => {
+        let {marca,img,descripcion,precio,descuento} = req.body
+
+        let productosAgregados = {
+            marca: marca,
+            img: [img],
+            detalle: descripcion,
+            precio: precio,
+            descuento: descuento,
+        }
+      
+
+  } 
 }
