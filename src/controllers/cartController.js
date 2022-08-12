db7a7ff0536b46f662068a4e8f4934138e0826f4
nachorm96 <Rubio.nacho@yahoo.com.ar>
2022-08-12 18:23:47 -0300

    retocando cartController

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,21 +2,16 @@ const fs = require('fs')
 const path = require('path')
 
 const save = (dato) => fs.writeFileSync(path.join(__dirname, '../data/carrito.json'),JSON.stringify(dato,null,4),'utf-8')
+const carrito = require('../data/carrito.json')
 module.exports = {
     cart : (req , res) => {
         res.render ('cart')
     },
     productosSeleccionados: (req,res) => {
-        let {marca,img,descripcion,precio,descuento} = req.body
-
-        let productosAgregados = {
-            marca: marca,
-            img: [img],
-            detalle: descripcion,
-            precio: precio,
-            descuento: descuento,
-        }
-      
-
-  } 
+        let id = req.params.id
+      if(carrito.id == id){
+        return carrito
+      }
+      res.render('cart')
+     } 
 }
