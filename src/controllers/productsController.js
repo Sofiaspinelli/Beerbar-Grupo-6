44304f08ad070bb46d4fcdb738221af831294f58
German704 <germaninsaurralde704@gmail.com>
2022-08-12 21:37:09 -0300

    agregando mejoras

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,13 +2,14 @@ const productos = require('../data/productos.json')
 
 module.exports = {
     products : (req, res) => {
+        // let descuentos = productos[i].precio - (productos[i].precio * productos[i].descuento / 100);
         res.render ('products', {productos})
     },
     detail: (req, res) => {
         let id = +req.params.id;
         let producto = productos.find(producto => producto.id == id); 
-
-        res.render('detail', {producto});
+        let descuentos = producto.precio - (producto.precio * producto.descuento / 100);
+        res.render('detail', {producto, descuentos});
     },
     cart : (req, res) => {
         res.render ('cart')
