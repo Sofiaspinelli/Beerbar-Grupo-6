fa97eb44e39f65ab432f59502d7515053f8d36a7
German704 <germaninsaurralde704@gmail.com>
2022-08-26 19:35:30 -0300

    arreglando vista detalle de productos

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -7,15 +7,22 @@ module.exports = {
         // let descuentos = productos[i].precio - (productos[i].precio * productos[i].descuento / 100);
         res.render ('products', {productos, tipoDeProd, proComidas})
     },
-    detail: (req, res) => {
+    detailB: (req, res) => {
         let id = +req.params.id;
         let tipoDeProd = ["birra", "comidas"]
-        let producto = productos.find(producto1 => producto1.id == id); 
-        let comidas = proComidas.find(producto2 => producto2.id == id); 
+        let producto = productos.find(producto => producto.id == id); 
         // let descuentos = producto.precio - (producto.precio * producto.descuento / 100);
-        // let descuentos2 = comidas.precio - (comidas.precio * comidas.descuento / 100);
         
-        res.render('detail', {producto, comidas, tipoDeProd});
+        res.render('detailB', {producto, tipoDeProd});
+
+    },
+    detailC: (req, res) => {
+        let id = +req.params.id;
+        let tipoDeProd = ["birra", "comidas"]
+        let comida = proComidas.find(comida => comida.id == id); 
+        // let descuentos = comidas.precio - (comidas.precio * comidas.descuento / 100);
+        
+        res.render('detailC', {comida, tipoDeProd});
 
     },
     cart : (req, res) => {
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -1,10 +1,11 @@
-const {products,cart,detail} = require ('../controllers/productsController');
+const {products,cart,detailB, detailC} = require ('../controllers/productsController');
 const express = require ('express');
 const router = express.Router();
 
 /* GET products page. */
 router.get('/', products);
-router.get('/:detail/:id', detail);
+router.get('/birra/:id', detailB);
+router.get('/comida/:id', detailC);
 router.get('/cart', cart);
 
 module.exports = router;
