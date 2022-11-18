2e833ad5d002b2ae0b05d0360900565a7070582d
German704 <germaninsaurralde704@gmail.com>
2022-11-18 16:18:54 -0300

    terminando vista de productos

--- beerbar/src/components/Article.jsx
+++ beerbar/src/components/Article.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Article() {
-return (
+function Article(id, nombre, imagen, detalle, precio, descuento) {
+    return (
     
-    <div>
-    <img src="/img/productos/ " alt="" />
-    <div class="contenedor">
-        <h3> </h3>
-        
-        <p class="old"></p>
-        <p class="precio"> <small class="descuento"></small> </p>
-        
-        
-        <p class="precio"></p>
-        
-    </div>
-    <button><a href="/products/detail/">Comprar</a></button>
-</div> 
+        <div>
+        <img src={`/img/productos/${imagen}`} alt="" />
+        <div class="contenedor">
+            <h3>{nombre}</h3>
 
-)
-
+            <p class="old">{precio}</p>
+            <p class="precio">${precio - (precio * descuento / 100)}<small class="descuento">{descuento}</small></p>
+            
+            
+            <p class="precio">${precio}</p>
+        
+        </div>
+        <button><a href={`/products/detail/${id}`}>Comprar</a></button>
+    </div> 
+    
+    )
 }
 
+Article.propTypes = {
+    id: PropTypes.number,
+    nombre: PropTypes.string,
+    marca: PropTypes.string,
+    imagen: PropTypes.string,
+    type: PropTypes.number,
+    detalle: PropTypes.string,
+    categoria: PropTypes.number,
+    precio: PropTypes.number,
+    descuento: PropTypes.number,
+    stock: PropTypes.number,
+    
+}
+// Article.defaultProps{
 
+// }
 export default Article
--- beerbar/src/components/Main.jsx
+++ beerbar/src/components/Main.jsx
@@ -2,14 +2,68 @@ import React from 'react'
 import Article from './Article'
 
 function Main() {
+  let productos =[
+    {
+      id:1,
+      nombre:"Pizza",
+      marca:"nafa",
+      detalle:"deliciosa",
+      precio:1000,
+      descuento:10,
+      stock:20,
+      categoria:"ninguno",
+      tipo: "comida",
+      imagen: "img-1662398149075.png"
+    },
+    {
+      id:1,
+      nombre:"Pizza",
+      marca:"nafa",
+      detalle:"deliciosa",
+      precio:1000,
+      descuento:10,
+      stock:20,
+      categoria:"ninguno",
+      tipo: "comida",
+      imagen: "img-1662398149075.png"
+    },
+    {
+      id:1,
+      nombre:"Pizza",
+      marca:"nafa",
+      detalle:"deliciosa",
+      precio:1000,
+      descuento:10,
+      stock:20,
+      categoria:"ninguno",
+      tipo: "comida",
+      imagen: "img-1662398149075.png"
+    }
+  ] 
   return (
     <main>
 
         <h1>PROMOCIONES</h1>
 
         <section class="productos">
-      
-            <Article ></Article>   
+          {productos.map((producto, index) => 
+
+            <Article
+            key = {index}
+            id = {producto.id}
+            nombre = {producto.nombre}
+            marca = {producto.marca}
+            detalle = {producto.detalle}
+            precio = {producto.precio}
+            descuento = {producto.descuento}
+            stock = {producto.stock}
+            categoria = {producto.categoria}
+            tipo = {producto.tipo}
+            imagen = {producto.imagen}
+            />
+
+          )}
+              
           
         </section>
 
