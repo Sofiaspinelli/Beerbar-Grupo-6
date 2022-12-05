894999afb1b94d71b3429e5a7095dbc35c2192a5
Sofiaspinelli <sofiaspinelli17@gmail.com>
2022-12-05 13:51:27 -0300

    vista register en proceso

--- site/public/js/validacionRegister.js
+++ site/public/js/validacionRegister.js
@@ -177,8 +177,8 @@ window.addEventListener('load', () => {
     iconEye.addEventListener("click",function() {
         const icon = $("#i");
         
-        if (this.nextElementSibling.type === "password"){
-            this.nextElementSibling.type = "text";
+        if (this.nextElementSibling.type === 'password'){
+            this.nextElementSibling.type = 'text';
             icon.classList.remove('fa-eye-slash')
             icon.classList.add('fa-eye')
         }else {
