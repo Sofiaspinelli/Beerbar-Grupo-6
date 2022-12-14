193b9f9fdd82a94b2eb3538a0c7e53efcca5fc2e
Sofiaspinelli <sofiaspinelli17@gmail.com>
2022-12-14 15:17:40 -0300

    mi primer commit

--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -1,8 +1,9 @@
-const {home, search} = require ('../controllers/indexController')
+const {home, search, contacto} = require ('../controllers/indexController')
 const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', home);
 router.get('/busqueda', search);
+/* router.get('/contacto', contacto); */
 module.exports = router;
