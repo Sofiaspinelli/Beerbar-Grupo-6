1345fcc09494f9793e73a18b406f538afcd10277
German704 <germaninsaurralde704@gmail.com>
2022-12-18 15:52:51 -0300

    perfil terminado

--- site/public/js/menu.js
+++ site/public/js/menu.js
@@ -4,9 +4,17 @@ window.addEventListener('load', () => {
 
     const menu = $('#menu');
     const nav = $('nav')
+    const miPerfil = $('#miPerfil')
+    const opciones = $('#opcionesPerfil')
 
     menu.addEventListener('click', () => {
         console.log('click en menu')
         nav.classList.toggle('desaparecer')
     })
+
+    miPerfil.addEventListener('click', () => {
+        console.log('click en perfil')
+        opciones.classList.toggle('blockOption')
+    })
+
 })
