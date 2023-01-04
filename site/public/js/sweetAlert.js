b6daa40ce84d61d834fefc889e135565e88cda95
Sofiaspinelli <sofiaspinelli17@gmail.com>
2023-01-04 02:07:42 -0300

    sweetalert en crud eliminar en proceso

---
+++ site/public/js/sweetAlert.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', () => {
+
+    
+    let forms = document.querySelectorAll('form');
+    for (let i = 0; i < forms.length; i++) {
+        forms[i].addEventListener('submit', event => {
+                event.preventDefault();
+                Swal.fire({
+                customClass: {
+                    confirmButton: 'swalBtnColor',
+                    cancelButton: 'swalBtnColor'
+                },
+
+                title: '¿Estas seguro que quieres eliminar el producto?',
+                text: "Esta accion es irreversible!",
+                icon: 'warning',
+                background: "#ebebeb",
+                showCancelButton: true,
+                confirmButtonColor: '#7ff77f',
+                cancelButtonColor: '#cc4141',
+                confirmButtonText: 'Eliminar',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+
+                }).then((result) => {
+
+                    if (result.isConfirmed) {
+                        forms[i].submit();
+                    }
+
+                })
+        })
+    }
+})
+
